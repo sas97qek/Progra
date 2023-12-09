e7b843373e25c3f4f8017ea4a5c404804cc284fd
Will Shf <willi_schefner@yahoo.de>
2023-12-10 00:05:18 +0100

    ship

---
+++ Ship.java
@@ -0,0 +1,20 @@
+public class Ship{
+
+    public static void main(String[] args){
+        StdDraw.setScale(0, 10);
+
+        StdDraw.setPenRadius(0.01);
+        StdDraw.line(1,3,9,3);
+        StdDraw.line(3,1,7,1);
+        StdDraw.line(3,1,1,3);
+        StdDraw.line(7,1,9,3);
+        StdDraw.line(5,3,5,9);
+        StdDraw.line(5,9,2,5);
+        StdDraw.line(2,5,5,5);
+    }
+
+
+
+
+
+}
